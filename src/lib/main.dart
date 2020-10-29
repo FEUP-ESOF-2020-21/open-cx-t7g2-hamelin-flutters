3ce1083edea4bdb1895a28127b7acc3a7cf0005a
rafaavc <rafavc2508@gmail.com>
2020-10-29 19:12:53 +0000

    Added routes and main screen; Started implementing the register screen

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -2,6 +2,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,30 +13,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Confnect',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: Colors.white,
-        primarySwatch: Colors.grey,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Confnect'),
-    );
+        title: 'Confnect',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primaryColor: Colors.white,
+          primarySwatch: Colors.grey,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'Confnect'),
+        routes: <String, WidgetBuilder>{
+          '/login': (context) => Login(),
+          '/register': (context) => Register()
+        });
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -50,23 +54,98 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          children: <Widget>[
+            Container(
+                alignment: Alignment.topLeft,
+                height: 540,
+                margin: EdgeInsets.only(
+                  top: 60,
+                  left: 30.0,
+                ),
+                child: Text(this.title,
+                    textScaleFactor: 2.5,
+                    style: GoogleFonts.lora(fontWeight: FontWeight.w600))),
+            Container(
+              alignment: Alignment.topLeft,
+              height: 60,
+              width: MediaQuery.of(context).size.width - 60,
+              margin: EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: ElevatedButton(
+                child: Text("Login"),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  minimumSize: MaterialStateProperty.all<Size>(
+                    Size(MediaQuery.of(context).size.width - 60, 50),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              height: 60,
+              width: MediaQuery.of(context).size.width - 60,
+              margin: EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: ElevatedButton(
+                child: Text("Register"),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register');
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  minimumSize: MaterialStateProperty.all<Size>(
+                    Size(MediaQuery.of(context).size.width - 60, 50),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Register extends StatefulWidget {
+  Register({Key key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  @override
+  _RegisterState createState() => _RegisterState();
+}
 
+class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -97,13 +176,12 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Container(
               alignment: Alignment.topLeft,
-              margin: EdgeInsets.only(
-                top: 50.0,
-                left: 15.0,
-              ),
+              margin: EdgeInsets.only(top: 50.0, left: 15.0, bottom: 100),
               child: IconButton(
                 icon: Icon(Icons.keyboard_return),
-                onPressed: _incrementCounter,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
             ),
             Container(
@@ -112,13 +190,222 @@ class _MyHomePageState extends State<MyHomePage> {
                 top: 20,
                 left: 30.0,
               ),
-              child: Text(
-                "Login",
-                textScaleFactor: 2.5,
-                style: TextStyle(
-                    //fontFamily: for another font,
-                    ),
+              child: Text("Register",
+                  textScaleFactor: 2.5,
+                  style: GoogleFonts.lora(fontWeight: FontWeight.w600)),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              height: 60,
+              width: MediaQuery.of(context).size.width - 60,
+              margin: EdgeInsets.only(
+                top: 30.0,
               ),
+              child: TextField(
+                selectionWidthStyle: BoxWidthStyle.max,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(
+                    top: 10.0,
+                    left: 10.0,
+                  ),
+                  hintText: "Full Name",
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      5,
+                    ),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.black,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      5,
+                    ),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              height: 60,
+              width: MediaQuery.of(context).size.width - 60,
+              margin: EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: TextField(
+                selectionWidthStyle: BoxWidthStyle.max,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(
+                    top: 10.0,
+                    left: 10.0,
+                  ),
+                  hintText: "Username",
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      5,
+                    ),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.black,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      5,
+                    ),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              height: 60,
+              width: MediaQuery.of(context).size.width - 60,
+              margin: EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: TextField(
+                selectionWidthStyle: BoxWidthStyle.max,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(
+                    top: 10.0,
+                    left: 10.0,
+                  ),
+                  hintText: "Password",
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      5,
+                    ),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.black,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      5,
+                    ),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              height: 90,
+              width: MediaQuery.of(context).size.width - 60,
+              margin: EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: ElevatedButton(
+                child: Text("Register"),
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  minimumSize: MaterialStateProperty.all<Size>(
+                    Size(MediaQuery.of(context).size.width - 60, 50),
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              height: 1,
+              color: Colors.black,
+              thickness: 1.5,
+              indent: 50,
+              endIndent: 50,
+            ),
+            Container(
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.popAndPushNamed(context, '/login');
+                },
+                child: Text(
+                  "Already have an account? Click Here!",
+                  style: TextStyle(
+                    //fontFamily: for another font,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  Login({Key key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(top: 50.0, left: 15.0, bottom: 100),
+              child: IconButton(
+                icon: Icon(Icons.keyboard_return),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(
+                top: 20,
+                left: 30.0,
+              ),
+              child: Text("Login",
+                  textScaleFactor: 2.5,
+                  style: GoogleFonts.lora(fontWeight: FontWeight.w600)),
             ),
             Container(
               alignment: Alignment.topLeft,
@@ -222,7 +509,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Container(
               child: FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.popAndPushNamed(context, '/register');
+                },
                 child: Text(
                   "Don't have an account? Click Here!",
                   style: TextStyle(
