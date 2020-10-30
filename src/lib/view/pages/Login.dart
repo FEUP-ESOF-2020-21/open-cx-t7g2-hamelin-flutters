1c355626016b9762c129a797ba84e1edd1e24344
rafaavc <rafavc2508@gmail.com>
2020-10-30 20:31:05 +0000

    Started forking widgets from pages; Forked FormFieldContainer, FormTextField and SquareButton

--- src/lib/view/pages/Login.dart
+++ src/lib/view/pages/Login.dart
@@ -1,10 +1,14 @@
 import 'dart:ui';
 
+import 'package:confnect/view/widgets/FormFieldContainer.dart';
+import 'package:confnect/view/widgets/FormTextField.dart';
+
 import '../AppRouter.dart';
 import '../Page.dart';
 import '../../controller/Controller.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../widgets/SquareButton.dart';
 
 class Login extends StatefulPage {
   Login(Controller controller, {Key key}) : super(controller, key: key);
@@ -16,31 +20,9 @@ class Login extends StatefulPage {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           children: <Widget>[
             Container(
               alignment: Alignment.topLeft,
@@ -57,104 +39,20 @@ class _LoginState extends State<Login> {
               margin: EdgeInsets.only(
                 top: 20,
                 left: 30.0,
+                bottom: 30,
               ),
               child: Text("Login",
                   textScaleFactor: 2.5,
                   style: GoogleFonts.lora(fontWeight: FontWeight.w600)),
             ),
-            Container(
-              alignment: Alignment.topLeft,
-              height: 60,
-              width: MediaQuery.of(context).size.width - 60,
-              margin: EdgeInsets.only(
-                top: 30.0,
-              ),
-              child: TextField(
-                selectionWidthStyle: BoxWidthStyle.max,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(
-                    top: 10.0,
-                    left: 10.0,
-                  ),
-                  hintText: "Username",
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(
-                      5,
-                    ),
-                    borderSide: BorderSide(
-                      width: 3,
-                      color: Colors.black,
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(
-                      5,
-                    ),
-                    borderSide: BorderSide(
-                      width: 3,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ),
+            FormFieldContainer(
+              FormTextField('Username'),
             ),
-            Container(
-              alignment: Alignment.topLeft,
-              height: 60,
-              width: MediaQuery.of(context).size.width - 60,
-              margin: EdgeInsets.only(
-                top: 10.0,
-              ),
-              child: TextField(
-                selectionWidthStyle: BoxWidthStyle.max,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(
-                    top: 10.0,
-                    left: 10.0,
-                  ),
-                  hintText: "Password",
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(
-                      5,
-                    ),
-                    borderSide: BorderSide(
-                      width: 3,
-                      color: Colors.black,
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(
-                      5,
-                    ),
-                    borderSide: BorderSide(
-                      width: 3,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ),
+            FormFieldContainer(
+              FormTextField('Password'),
             ),
-            Container(
-              alignment: Alignment.topLeft,
-              height: 90,
-              width: MediaQuery.of(context).size.width - 60,
-              margin: EdgeInsets.only(
-                top: 10.0,
-              ),
-              child: ElevatedButton(
-                child: Text("Login"),
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.black),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  minimumSize: MaterialStateProperty.all<Size>(
-                    Size(MediaQuery.of(context).size.width - 60, 50),
-                  ),
-                ),
-              ),
-            ),
+            FormFieldContainer(SquareButton('Login', () {}),
+                margin: EdgeInsets.only(bottom: 30)),
             Divider(
               height: 1,
               color: Colors.black,
--- src/lib/view/pages/WelcomeScreen.dart
+++ src/lib/view/pages/WelcomeScreen.dart
@@ -1,5 +1,8 @@
 import 'package:confnect/controller/Controller.dart';
+import 'package:confnect/model/AppModel.dart';
 import 'package:confnect/view/Page.dart';
+import 'package:confnect/view/widgets/FormFieldContainer.dart';
+import 'package:confnect/view/widgets/SquareButton.dart';
 
 import '../AppRouter.dart';
 import 'package:flutter/material.dart';
@@ -19,31 +22,11 @@ class WelcomeScreen extends StatelessPage {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    AppModel model = super.getController().getModel();
+
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           children: <Widget>[
             Container(
                 alignment: Alignment.topLeft,
@@ -51,49 +34,20 @@ class WelcomeScreen extends StatelessPage {
                   top: 60,
                   left: 30.0,
                 ),
-                child: Text(super.getController().getModel().getAppName(),
+                child: Text(model.getAppName(),
                     textScaleFactor: 2.5,
                     style: GoogleFonts.lora(fontWeight: FontWeight.w600))),
             Spacer(),
-            Container(
-              height: 60,
-              width: MediaQuery.of(context).size.width - 60,
-              margin: EdgeInsets.only(bottom: 20.0),
-              child: ElevatedButton(
-                child: Text("Login"),
-                onPressed: () {
-                  Navigator.pushNamed(context, AppRouter.LOGIN);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.black),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  minimumSize: MaterialStateProperty.all<Size>(
-                    Size(MediaQuery.of(context).size.width - 60, 50),
-                  ),
-                ),
-              ),
+            FormFieldContainer(
+              SquareButton('Login', () {
+                Navigator.pushNamed(context, AppRouter.LOGIN);
+              }),
             ),
-            Container(
-              height: 60,
-              width: MediaQuery.of(context).size.width - 60,
-              margin: EdgeInsets.only(bottom: 30.0),
-              child: ElevatedButton(
-                child: Text("Register"),
-                onPressed: () {
-                  Navigator.pushNamed(context, AppRouter.REGISTER);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.black),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  minimumSize: MaterialStateProperty.all<Size>(
-                    Size(MediaQuery.of(context).size.width - 60, 50),
-                  ),
-                ),
-              ),
+            FormFieldContainer(
+              SquareButton('Register', () {
+                Navigator.pushNamed(context, AppRouter.REGISTER);
+              }),
+              margin: EdgeInsets.only(bottom: 30),
             ),
           ],
         ),
