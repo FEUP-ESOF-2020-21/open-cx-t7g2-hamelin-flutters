ccdc0456bf109ac70520d962cf76bacf3ad2d92f
Xavier-Pisco <x.pisco@gmail.com>
2020-11-29 18:56:07 +0000

    Changed validator to accept non required fields but with minimum and maximum lenght

--- src/lib/controller/ValidatorFactory.dart
+++ src/lib/controller/ValidatorFactory.dart
@@ -5,6 +5,35 @@ class ValidatorFactory {
       int upperLimit = -1,
       Function extender}) {
     String Function(String value) f = (value) {
+      if (fieldRequired) {
+        if (value.length == 0) {
+          return fieldName + " can not be empty";
+        } else if (lowerLimit != -1 && value.toString().length < lowerLimit) {
+          return fieldName +
+              " may not have less than " +
+              lowerLimit.toString() +
+              " characters";
+        } else if (upperLimit != -1 && value.toString().length > upperLimit) {
+          return fieldName +
+              " may not have more than " +
+              upperLimit.toString() +
+              " characters";
+        }
+      } else if (value.toString().length != 0) {
+        // Se não for obrigatório ter mas existir tem de cumprir os requesitos
+        if (lowerLimit != -1 && value.toString().length < lowerLimit) {
+          return fieldName +
+              " may not have less than " +
+              lowerLimit.toString() +
+              " characters";
+        } else if (upperLimit != -1 && value.toString().length > upperLimit) {
+          return fieldName +
+              " may not have more than " +
+              upperLimit.toString() +
+              " characters";
+        }
+      }
+      /*
       if (fieldRequired && value.length == 0) {
         return fieldName + " can not be empty";
       } else if (fieldRequired &&
@@ -21,7 +50,7 @@ class ValidatorFactory {
             " may not have more than " +
             upperLimit.toString() +
             " characters";
-      }
+      }*/
       if (extender == null) return null;
       return extender(value);
     };
