b976aa4cda5856fb085a474645f7b8e05f1ac35a
TsarkFC <up201806779@fe.up.pt>
2020-11-30 18:17:36 +0000

    Register validators implemented

--- src/lib/view/pages/Register.dart
+++ src/lib/view/pages/Register.dart
@@ -32,73 +32,77 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     Database db = _controller.getDatabase();
     return Scaffold(
-      body: ListView(
-        children: [
-          GoBackButton(),
-          Spacer(
-            flex: 1,
-          ),
-          PageTitle(
-            "Register",
-            margin: EdgeInsets.only(
-              top: 20,
-              left: 30.0,
-              bottom: 30,
-            ),
-          ),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                FormFieldContainer(
-                  FormTextField('Full Name', fullNameController,
-                      validator: ValidatorFactory.getValidator('Full name',
-                          fieldRequired: true)),
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              GoBackButton(),
+              Spacer(
+                flex: 1,
+              ),
+              PageTitle(
+                "Register",
+                margin: EdgeInsets.only(
+                  top: 20,
+                  left: 30.0,
+                  bottom: 30,
                 ),
-                FormFieldContainer(FormTextField('Username', usernameController,
-                    validator: ValidatorFactory.getValidator('Full name',
-                        fieldRequired: true, extender: (value) {
-                      if (db.existsUser(value)) {
-                        return "User with username " +
-                            value.toString() +
-                            " already exists!";
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    FormFieldContainer(
+                      FormTextField('Full Name', fullNameController,
+                          validator: ValidatorFactory.getValidator('Full name',
+                              fieldRequired: true)),
+                    ),
+                    FormFieldContainer(FormTextField(
+                        'Username', usernameController,
+                        validator: ValidatorFactory.getValidator('Full name',
+                            fieldRequired: true, extender: (value) {
+                          if (db.existsUser(value)) {
+                            return "User with username " +
+                                value.toString() +
+                                " already exists!";
+                          }
+                        }))),
+                    FormFieldContainer(
+                      FormTextField('Password', passwordController,
+                          obscureText: true,
+                          validator: ValidatorFactory.getValidator('Full name',
+                              fieldRequired: true)),
+                      margin: EdgeInsets.only(
+                        bottom: 30,
+                      ),
+                    ),
+                    FormFieldContainer(SquareButton('Register', () {
+                      if (_formKey.currentState.validate()) {
+                        String fullname = fullNameController.text,
+                            username = usernameController.text,
+                            password = passwordController.text;
+                        //userImageURL = talkImageURLController.text;
+                        db.register(fullname, username, password);
+                        _controller.setLoggedInUserName(username);
+                        Navigator.popUntil(context,
+                            ModalRoute.withName(Navigator.defaultRouteName));
                       }
-                    }))),
-                FormFieldContainer(
-                  FormTextField('Password', passwordController,
-                      obscureText: true,
-                      validator: ValidatorFactory.getValidator('Full name',
-                          fieldRequired: true)),
-                  margin: EdgeInsets.only(
-                    bottom: 30,
-                  ),
+                    })),
+                  ],
                 ),
-                FormFieldContainer(SquareButton('Register', () {
-                  if (_formKey.currentState.validate()) {
-                    String fullname = fullNameController.text,
-                        username = usernameController.text,
-                        password = passwordController.text;
-                    //userImageURL = talkImageURLController.text;
-                    db.register(fullname, username, password);
-                    //_onTalkAdded();
-                    Navigator.pop(context);
-                  }
-                })),
-              ],
-            ),
+              ),
+              StandardDivider(),
+              TextOnlyButton(
+                () {
+                  Navigator.popAndPushNamed(context, AppRouter.LOGIN);
+                },
+                text: "Already have an account? Click Here!",
+              ),
+              Spacer(
+                flex: 2,
+              )
+            ],
           ),
-          StandardDivider(),
-          TextOnlyButton(
-            () {
-              Navigator.popAndPushNamed(context, AppRouter.LOGIN);
-            },
-            text: "Already have an account? Click Here!",
-          ),
-          Spacer(
-            flex: 2,
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
