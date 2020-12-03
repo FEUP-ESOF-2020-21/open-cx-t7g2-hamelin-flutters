21e904414c96142f975058a7a66106d54b587310
Joao Rocha <jr007silva1306@gmail.com>
2020-12-03 18:23:13 +0000

    Interest Suggestions now working

--- src/lib/model/User.dart
+++ src/lib/model/User.dart
@@ -1,7 +1,13 @@
+import 'Tag.dart';
+import 'forums/Forum.dart';
+import 'forums/TagForum.dart';
+
 class User {
   final int id;
   final String _fullname, _username, _password, _role;
   String _avatarUrl;
+  List<Forum> _forums = new List();
+  List<Tag> _tags = new List();
   User(this.id, this._role, this._fullname, this._username, this._password,
       [this._avatarUrl]);
 
@@ -19,6 +25,25 @@ class User {
 
   void setAvatarUrl(String avatarUrl) {
     this._avatarUrl = avatarUrl;
+  }
+
+  List<Forum> getForums() {
+    return _forums;
+  }
+
+  void addForum(Forum forum) {
+    if(!this._forums.contains(forum)) {
+      this._forums.add(forum);
+      /*forum.getTags().forEach((tag) {
+        !this._tags.contains(tag) ?? this._tags.add(tag);
+      });*/
+      print("User Tags: " + forum.getTags().toString());
+      this._tags.addAll(forum.getTags());
+    }
+  }
+
+  List<Tag> getTags() {
+    return _tags;
   }
 
   bool auth(String username, String password) {
---
+++ src/lib/view/widgets/forum/AllForumList.dart
@@ -0,0 +1,94 @@
+import 'package:confnect/controller/Controller.dart';
+import 'package:confnect/controller/database/Database.dart';
+import 'package:confnect/model/forums/Forum.dart';
+import 'package:confnect/model/Tag.dart';
+import 'package:confnect/model/User.dart';
+import 'package:confnect/view/Page.dart';
+import './ForumTile.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AllForumList extends StatelessPage {
+  final Function _viewForum;
+  List<Forum> _suggestedForums = new List();
+
+  AllForumList(Controller controller, this._viewForum, {Key key})
+      : super(controller, key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //final Users users = Provider.of(context);
+    return ListView(
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+          child: Text(
+            'Suggested Forums',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+                fontFamily: 'Montserrat'),
+          ),
+        ),
+        ListView(children: suggestedForums(),
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics()),
+        Container(
+          margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+          child: Text(
+            'Other Forums',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+                fontFamily: 'Montserrat'),
+          ),
+        ),
+        ListView(children: otherforums(),
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics()),
+      ],
+    );
+  }
+
+  List<dynamic> otherforums() {
+    Database db = super.getController().getDatabase();
+
+    List<Forum> difference = db.getForums().toSet().difference(
+        _suggestedForums.toSet()).toList();
+
+    return difference.map((forum) => ForumTile(forum, _viewForum)).toList();
+  }
+
+  List<dynamic> suggestedForums() {
+    Database db = super.getController().getDatabase();
+    String username = super.getController().getLoggedInUserName();
+    User u = db.getUser(username);
+    List<Forum> _userForums = u.getForums();
+    List<Tag> _userTags = u.getTags();
+
+    print(_userForums);
+    print(_userTags);
+
+    _suggestedForums = new List();
+
+    db.getForums().forEach((forum) {
+      for (int i = 0; i < _userTags.length; i++) {
+
+        if (forum.getTags() == null)
+          continue;
+        print('FORUM TAGS : ' + forum.getTags().toString());
+        if (forum.getTags().contains(_userTags[i])) {
+          _suggestedForums.add(forum);
+          break;
+        }
+      }
+    });
+
+    print(_suggestedForums);
+
+    return _suggestedForums.map((forum) => ForumTile(forum, _viewForum))
+        .toList();
+  }
+}
