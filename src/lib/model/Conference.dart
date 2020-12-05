a7948cbab96842b8ea7832793bd4b5ed4b594008
rafaavc <rafavc2508@gmail.com>
2020-12-05 22:07:30 +0000

    Implemented Conference model

---
+++ src/lib/model/Conference.dart
@@ -0,0 +1,8 @@
+class Conference {
+  String _name, _description, _location;
+  Conference(this._name, this._description, this._location);
+
+  String getName() => _name;
+  String detDescription() => _description;
+  String getLocation() => _location;
+}
