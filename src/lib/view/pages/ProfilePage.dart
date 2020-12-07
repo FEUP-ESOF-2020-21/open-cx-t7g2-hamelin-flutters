0a13921ff9426d5a512963a55596382155727777
TsarkFC <up201806779@fe.up.pt>
2020-12-07 16:10:00 +0000

    Minor adjust in profile app bar

--- src/lib/view/pages/ProfilePage.dart
+++ src/lib/view/pages/ProfilePage.dart
@@ -43,122 +43,134 @@ class _ProfilePageState extends State<ProfilePage> {
         HorizontalForumList(_controller, viewForumAction, _userForums);
 
     return Scaffold(
-        //appBar: widget.user == null ? null : AppBar(),
+        appBar: widget.user == null ? null : AppBar(),
         body: Stack(
-      children: <Widget>[
-        Positioned(
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              children: <Widget>[
-                Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height / 3.0,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/main_page_app_bar.jpg'),
-                            fit: BoxFit.cover)),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height / 17.5),
-                        Container(
-                            width: MediaQuery.of(context).size.width / 3.0,
-                            height: MediaQuery.of(context).size.height / 6.0,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: NetworkImage(
-                                        _viewingUser.getAvatarUrl()),
-                                    fit: BoxFit.cover),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(75.0)),
-                                border:
-                                    Border.all(color: Colors.white, width: 5))),
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height / 200.0),
-                        Text(_viewingUser.getFullName(),
-                            style: TextStyle(
-                                fontSize: 30.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white)),
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height / 400.0),
-                        SizedBox(
-                            width: MediaQuery.of(context).size.width / 1.5,
-                            child: Center(
-                                child: Text("@" + _viewingUser.getUsername(),
-                                    style: TextStyle(
-                                        fontSize: 17.0, color: Colors.white)))),
-                      ],
-                    )),
-                SizedBox(height: MediaQuery.of(context).size.height / 50.0),
-                SizedBox(
-                    width: MediaQuery.of(context).size.width / 1.5,
-                    child: Center(
-                        child: Text(_viewingUser.getBio(),
-                            style: TextStyle(fontSize: 17.0)))),
-                SizedBox(height: MediaQuery.of(context).size.height / 35.0),
-                _viewingUser == _controller.getLoggedInUser()
-                    ? Container(
-                        height: MediaQuery.of(context).size.height / 31.0,
-                        width: MediaQuery.of(context).size.width / 4.0,
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.0),
-                          shadowColor: Colors.greenAccent,
-                          color: Colors.green,
-                          elevation: 7.0,
-                          child: OutlineButton(
-                            child: Text(
-                              "Edit profile",
-                              style: TextStyle(
-                                  fontSize:
-                                      MediaQuery.of(context).size.width / 30.0,
-                                  color: Colors.white),
-                            ),
-                            highlightedBorderColor: Colors.green,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15)),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => EditProfilePage(
-                                        _controller, _refreshProfileState)),
-                              );
-                            },
+          children: <Widget>[
+            Positioned(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height / 3.0,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image:
+                                    AssetImage('assets/main_page_app_bar.jpg'),
+                                fit: BoxFit.cover)),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height / 17.5),
+                            Container(
+                                width: MediaQuery.of(context).size.width / 3.0,
+                                height:
+                                    MediaQuery.of(context).size.height / 6.0,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            _viewingUser.getAvatarUrl()),
+                                        fit: BoxFit.cover),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(75.0)),
+                                    border: Border.all(
+                                        color: Colors.white, width: 5))),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height / 200.0),
+                            Text(_viewingUser.getFullName(),
+                                style: TextStyle(
+                                    fontSize: 30.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height / 400.0),
+                            SizedBox(
+                                width: MediaQuery.of(context).size.width / 1.5,
+                                child: Center(
+                                    child: Text(
+                                        "@" + _viewingUser.getUsername(),
+                                        style: TextStyle(
+                                            fontSize: 17.0,
+                                            color: Colors.white)))),
+                          ],
+                        )),
+                    SizedBox(height: MediaQuery.of(context).size.height / 50.0),
+                    SizedBox(
+                        width: MediaQuery.of(context).size.width / 1.5,
+                        child: Center(
+                            child: Text(_viewingUser.getBio(),
+                                style: TextStyle(fontSize: 17.0)))),
+                    SizedBox(height: MediaQuery.of(context).size.height / 35.0),
+                    _viewingUser == _controller.getLoggedInUser()
+                        ? Container(
+                            height: MediaQuery.of(context).size.height / 31.0,
+                            width: MediaQuery.of(context).size.width / 4.0,
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20.0),
+                              shadowColor: Colors.greenAccent,
+                              color: Colors.green,
+                              elevation: 7.0,
+                              child: OutlineButton(
+                                child: Text(
+                                  "Edit profile",
+                                  style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.of(context).size.width /
+                                              30.0,
+                                      color: Colors.white),
+                                ),
+                                highlightedBorderColor: Colors.green,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15)),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => EditProfilePage(
+                                            _controller, _refreshProfileState)),
+                                  );
+                                },
+                              ),
+                            ))
+                        : Divider(),
+                    SizedBox(height: MediaQuery.of(context).size.height / 25.0),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Forums',
+                              textAlign: TextAlign.center,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontWeight: FontWeight.bold))
+                        ]),
+                    SizedBox(
+                        height: MediaQuery.of(context).size.height / 100.0),
+                    if (_userForums.length != 0)
+                      Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [profile]))
+                    else
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color: Colors.black,
+                            size: 50.0,
                           ),
-                        ))
-                    : Divider(),
-                SizedBox(height: MediaQuery.of(context).size.height / 25.0),
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Text('Forums',
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontWeight: FontWeight.bold))
-                ]),
-                SizedBox(height: MediaQuery.of(context).size.height / 100.0),
-                if (_userForums.length != 0)
-                  Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [profile]))
-                else
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.search,
-                        color: Colors.black,
-                        size: 50.0,
-                      ),
-                      Text(
-                        "No forums found...",
-                        style: TextStyle(fontSize: 20.0, color: Colors.black),
+                          Text(
+                            "No forums found...",
+                            style:
+                                TextStyle(fontSize: 20.0, color: Colors.black),
+                          )
+                        ],
                       )
-                    ],
-                  )
-              ],
-            ))
-      ],
-    ));
+                  ],
+                ))
+          ],
+        ));
   }
 }
